ff3c9a66f160f025e82bf2e443819d9740c05eac
bob <bobthekingofegypt@googlemail.com>
2012-01-14 17:38:30 +0000

    adding in default project layout

---
+++ BobUtils/BobUtils.h
@@ -0,0 +1,13 @@
+//
+//  BobUtils.h
+//  BobUtils
+//
+//  Created by Richard Martin on 14/01/2012.
+//  Copyright (c) 2012 Richard Martin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BobUtils : NSObject
+
+@end
---
+++ BobUtils/BobUtils.m
@@ -0,0 +1,23 @@
+//
+//  BobUtils.m
+//  BobUtils
+//
+//  Created by Richard Martin on 14/01/2012.
+//  Copyright (c) 2012 Richard Martin. All rights reserved.
+//
+
+#import "BobUtils.h"
+
+@implementation BobUtils
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
